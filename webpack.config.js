6ddb34f3ffd2e12993cc880c032d51c77b43f0c6
Fen1xx <provkovasu15@gmail.com>
2022-03-11 14:17:49 +0500

    Setting webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV = development;
+const isProd = !isDev;
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/[hash]',
+        clean: true,
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: isDev ? 'source-map' : false,
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    devServer: {
+        hot: false,
+        liveReload: true,
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.pug$/i,
+                use: 'pug-loader',
+                include: path.join(__dirname, 'src'),
+                exclude: /(node_modules|bower_components)/,
+            },
+            {
+                test: /\.m?js/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.pug',
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: isProd,
+            },
+        }),
+    ],
+};
