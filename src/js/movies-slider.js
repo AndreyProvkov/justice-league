7bfca3dbd74bfc9e0f866e8982982060d7199734
Fen1xx <provkovasu15@gmail.com>
2022-03-22 20:31:59 +0500

    Adding an adaptive for a "tv-shows"

--- src/js/movies-slider.js
+++ src/js/movies-slider.js
@@ -5,5 +5,27 @@ $(function(){
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,}
+            },
+            {
+                breakpoint: 967,
+                settings: {
+                    slidesToShow: 4,}
+            },
+            {
+                breakpoint: 676,
+                settings: {
+                    slidesToShow: 3,}
+            },
+            {
+                breakpoint: 467,
+                settings: {
+                    slidesToShow: 2,}
+            }
+        ]
     })
 });
