e509b50f89590498fc02892acf168785957c51ee
Fen1xx <provkovasu15@gmail.com>
2022-07-03 19:00:19 +0500

    Fix display when the screen size is reduced
    There was an empty space on the screen on the right side when the
    animation of appearance of elements had not yet begun

--- src/js/aosManual.js
+++ src/js/aosManual.js
@@ -1,13 +1,13 @@
 import AOS from 'aos';
 
-$(function() {
-     AOS.init ({ 
-        offset: 100, 
-        duration: 300, 
-        easing:"ease", 
-        once: true,
-        startEvent: 'DOMContentLoaded',
+AOS.init ({ 
+    offset: 100,
+    delay: 50,
+    duration: 300, 
+    easing:"ease", 
+    once: true,
+    startEvent: 'DOMContentLoaded',
+    disable: 'mobile',
+}); 
 
-    }); 
-    window.addEventListener('load', AOS.refresh); 
-});
+AOS.refresh();
